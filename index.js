020c939e0f2a7ed7ae20ab36301edc4077abc71f
randengrims <randengrimshaw@gmail.com>
2024-11-22 10:26:35 -0700

    test update

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.set("view engine", "ejs");
 
 app.set("views", path.join(__dirname, "views"));
 
-app.use(express.urlencoded({extended: true})); // 
+app.use(express.urlencoded({extended: true})); // hello!
 
 const knex = require("knex") ({
     client : "pg",
